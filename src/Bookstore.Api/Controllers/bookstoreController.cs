5b2e2102dce3141b33d3f4654167fd1cc5d0618c
Carlos A Santos <carlos.itdeveloper@gmail.com>
2022-01-26 21:39:52 -0300

    Copy Product to ProductDTO in ViewModels folder

--- src/Bookstore.Api/Controllers/bookstoreController.cs
+++ src/Bookstore.Api/Controllers/bookstoreController.cs
@@ -19,11 +19,11 @@ namespace APIBookstore.Api.Controllers
         {
             _context = context;
 
-            _context.TodoProducts.Add(new Product { Id = "1", Name = "Book1", Price = 24, Quantity = 1, Category = "action", Img = "Img1" });
-            _context.TodoProducts.Add(new Product { Id = "2", Name = "Book2", Price = 50, Quantity = 1, Category = "action", Img = "Img1" });
-            _context.TodoProducts.Add(new Product { Id = "3", Name = "Book3", Price = 20, Quantity = 2, Category = "action", Img = "Img1" });
-            _context.TodoProducts.Add(new Product { Id = "4", Name = "Book4", Price = 10, Quantity = 1, Category = "action", Img = "Img1" });
-            _context.TodoProducts.Add(new Product { Id = "5", Name = "Book5", Price = 15, Quantity = 5, Category = "action", Img = "Img1" });
+            _context.TodoProducts.Add(new ProductDTO { Id = "1", Name = "Book1", Price = 24, Quantity = 1, Category = "action", Img = "Img1" });
+            _context.TodoProducts.Add(new ProductDTO { Id = "2", Name = "Book2", Price = 50, Quantity = 1, Category = "action", Img = "Img1" });
+            _context.TodoProducts.Add(new ProductDTO { Id = "3", Name = "Book3", Price = 20, Quantity = 2, Category = "action", Img = "Img1" });
+            _context.TodoProducts.Add(new ProductDTO { Id = "4", Name = "Book4", Price = 10, Quantity = 1, Category = "action", Img = "Img1" });
+            _context.TodoProducts.Add(new ProductDTO { Id = "5", Name = "Book5", Price = 15, Quantity = 5, Category = "action", Img = "Img1" });
 
 
 
@@ -34,7 +34,7 @@ namespace APIBookstore.Api.Controllers
         }
 
         [HttpPost]
-        public async Task<ActionResult<Product>> PostProduct(Product product)
+        public async Task<ActionResult<ProductDTO>> PostProduct(ProductDTO product)
         {
             _context.TodoProducts.Add(product);
             await _context.SaveChangesAsync();
@@ -44,7 +44,7 @@ namespace APIBookstore.Api.Controllers
 
         // GET: api/
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<Product>>> GetTodoItems()
+        public async Task<ActionResult<IEnumerable<ProductDTO>>> GetTodoItems()
         {
             return await _context.TodoProducts.ToListAsync();
 
@@ -54,7 +54,7 @@ namespace APIBookstore.Api.Controllers
 
         // GET: api/bookstore/5
         [HttpGet("{id}")]
-        public async Task<ActionResult<Product>> GetProdut(int id)
+        public async Task<ActionResult<ProductDTO>> GetProdut(int id)
         {
             var todoItem = await _context.TodoProducts.FindAsync(id.ToString());
 
