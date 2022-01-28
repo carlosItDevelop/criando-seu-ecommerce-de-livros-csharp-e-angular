44f09566ab922fd6a11edbab1c3fbdbefa367c29
Carlos A Santos <carlos.itdeveloper@gmail.com>
2022-01-27 22:15:53 -0300

    IRepositoryProducts created

---
+++ src/Bookstore.Domain/Abstractions/Repository/IRepositoryProducts.cs
@@ -0,0 +1,9 @@
+﻿using Bookstore.Domain.Abstractions.Repository.Base;
+using Bookstore.Domain.Entities;
+
+namespace Bookstore.Domain.Abstractions.Repository
+{
+    public interface IRepositoryProducts : IGenericRepository<Product, int>
+    {
+    }
+}
