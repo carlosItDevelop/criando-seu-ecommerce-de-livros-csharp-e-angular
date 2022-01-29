bdd8c94b9c1f262e21ed047a9e75b0eb1452562f
Carlos A Santos <carlos.itdeveloper@gmail.com>
2022-01-28 21:42:19 -0300

    ConfigureSwaggerOptions, Versioning and DefaultValues in Extensions

--- src/Bookstore.Api/Configurations/Extensions/Swagger/SwaggerConfigVersioning.cs
+++ src/Bookstore.Api/Configurations/Extensions/Swagger/SwaggerConfigVersioning.cs
@@ -1,9 +1,7 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Mvc.ApiExplorer;
-using Microsoft.Extensions.Options;
-using Microsoft.OpenApi.Any;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.OpenApi.Models;
-using Swashbuckle.AspNetCore.SwaggerGen;
 
 namespace Cooperchip.DiretoAoPonto.UoW.Api.Configurations.Extensions
 {
@@ -57,82 +55,6 @@ namespace Cooperchip.DiretoAoPonto.UoW.Api.Configurations.Extensions
                     }
                 });
             return app;
-        }
-    }
-
-    public class ConfigureSwaggerOptions : IConfigureOptions<SwaggerGenOptions>
-    {
-        readonly IApiVersionDescriptionProvider provider;
-
-        public ConfigureSwaggerOptions(IApiVersionDescriptionProvider provider) => this.provider = provider;
-
-        public void Configure(SwaggerGenOptions options)
-        {
-            foreach (var description in provider.ApiVersionDescriptions)
-            {
-                options.SwaggerDoc(description.GroupName, CreateInfoForApiVersion(description));
-            }
-        }
-
-        static OpenApiInfo CreateInfoForApiVersion(ApiVersionDescription description)
-        {
-            var info = new OpenApiInfo()
-            {
-                Title = "API - Cooperchip",
-                Version = description.ApiVersion.ToString(),
-                Description = "Esta API faz parte do curso Asp.Net Core 6 - UoW.",
-                Contact = new OpenApiContact() { Name = "Carlos A Santos", Email = "contato.cooperchip@gmail.com" },
-                TermsOfService = new Uri("https://opensource.org/licenses/MIT"),
-                License = new OpenApiLicense() { Name = "MIT", Url = new Uri("https://opensource.org/licenses/MIT") }
-            };
-
-            if (description.IsDeprecated)
-            {
-                info.Description += " *** ESTA VERSÃO ESTÁ OBSOLETA ***";
-            }
-
-            return info;
-        }
-    }
-
-    public class SwaggerDefaultValues : IOperationFilter
-    {
-        public void Apply(OpenApiOperation operation, OperationFilterContext context)
-        {
-            if (operation.Parameters == null)
-            {
-                return;
-            }
-
-            foreach (var parameter in operation.Parameters)
-            {
-                var description = context.ApiDescription
-                    .ParameterDescriptions
-                    .First(p => p.Name == parameter.Name);
-
-                var routeInfo = description.RouteInfo;
-
-                operation.Deprecated = OpenApiOperation.DeprecatedDefault;
-
-                if (parameter.Description == null)
-                {
-                    parameter.Description = description.ModelMetadata?.Description;
-                }
-
-                if (routeInfo == null)
-                {
-                    continue;
-                }
-
-                if (parameter.In != ParameterLocation.Path && parameter.Schema.Default == null)
-                {
-                    parameter.Schema.Default = new OpenApiString(routeInfo.DefaultValue?.ToString());
-                }
-
-                parameter.Required |= !routeInfo.IsOptional;
-
-
-            }
         }
     }
 
