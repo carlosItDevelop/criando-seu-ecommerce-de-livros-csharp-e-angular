88c642fd6844cd9abf6a56d6e00d8ae89e543f32
Carlos A Santos <carlos.itdeveloper@gmail.com>
2022-01-28 21:58:29 -0300

    Versioned Controllers inherited from MainController and without ApiController Attributes

--- src/Bookstore.Api/v1/Controllers/BookstoreController.cs
+++ src/Bookstore.Api/v1/Controllers/BookstoreController.cs
@@ -1,4 +1,5 @@
-﻿using APIBookstore.Api.Dtos;
+﻿using APIBookstore.Api.Controllers;
+using APIBookstore.Api.Dtos;
 using AutoMapper;
 using Bookstore.Domain.Abstractions.Repository;
 using Bookstore.Domain.Entities;
@@ -7,16 +8,15 @@ using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
-namespace APIBookstore.Api.Controllers
+namespace APIBookstore.Api.v1.Controllers
 {
     [Route("api/[controller]")]
-    [ApiController]
-    public class BookstoreController : ControllerBase
+    public class BookstoreController : MainController
     {
         private readonly IRepositoryProducts _repoProducts;
         private readonly IMapper _mapper;
 
-        public BookstoreController(IRepositoryProducts repoProducts, 
+        public BookstoreController(IRepositoryProducts repoProducts,
                                    IMapper mapper)
         {
             _repoProducts = repoProducts;
@@ -28,7 +28,7 @@ namespace APIBookstore.Api.Controllers
         public async Task<IEnumerable<ProductDTO>> GetTodoItems()
         {
             //return await _context.TodoProducts.ToListAsync();
-            return  _mapper.Map<IEnumerable<ProductDTO>>(await _repoProducts.GetAll());
+            return _mapper.Map<IEnumerable<ProductDTO>>(await _repoProducts.GetAll());
         }
 
 
