607ff8cb96d5c5dadb600adf4a9c71262b7ce71d
Carlos A Santos <carlos.itdeveloper@gmail.com>
2022-01-28 21:52:15 -0300

    Copy BookstoreController to v1, v2 and v3

---
+++ src/Bookstore.Api/v2/Controllers/BookstoreController.cs
@@ -0,0 +1,81 @@
+﻿using APIBookstore.Api.Dtos;
+using AutoMapper;
+using Bookstore.Domain.Abstractions.Repository;
+using Bookstore.Domain.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace APIBookstore.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookstoreController : ControllerBase
+    {
+        private readonly IRepositoryProducts _repoProducts;
+        private readonly IMapper _mapper;
+
+        public BookstoreController(IRepositoryProducts repoProducts, 
+                                   IMapper mapper)
+        {
+            _repoProducts = repoProducts;
+            _mapper = mapper;
+        }
+
+        [Route("")]
+        [HttpGet("obter-todos")]
+        public async Task<IEnumerable<ProductDTO>> GetTodoItems()
+        {
+            //return await _context.TodoProducts.ToListAsync();
+            return  _mapper.Map<IEnumerable<ProductDTO>>(await _repoProducts.GetAll());
+        }
+
+
+        // GET: api/bookstore/5
+        [HttpGet("obter-produto/{id}")]
+        public async Task<ActionResult<ProductDTO>> GetProdut(int id)
+        {
+            //var todoItem = await _context.TodoProducts.FindAsync(id.ToString());
+            var todoItem = _mapper.Map<ProductDTO>(await _repoProducts.GetById(id));
+
+            if (todoItem == null)
+            {
+                return NotFound();
+            }
+
+            return todoItem;
+        }
+
+
+        [HttpPost("adicionar-produto")]
+        [ProducesResponseType(typeof(Product), StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<Product>> PostProduct(ProductDTO productDto)
+        {
+            //_context.TodoProducts.Add(product);
+            //await _context.SaveChangesAsync();
+
+            if (!ModelState.IsValid) return BadRequest("A Model está inválida!");
+
+            try
+            {
+                await _repoProducts.Add(_mapper.Map<Product>(productDto));
+                await _repoProducts.Commit();
+
+                return CreatedAtAction(nameof(GetProdut), new { id = productDto.Id }, productDto);
+            }
+            catch (System.Exception)
+            {
+                /* faça algo... 
+                 * avise a alguém 
+                 * ou não faça nada...
+                 */
+                await _repoProducts.Rollback();
+                return BadRequest("Erro ao tentar adicionar Produto");
+            }
+
+        }
+
+    }
+}
