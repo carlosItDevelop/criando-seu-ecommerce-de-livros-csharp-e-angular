bd57e622a3f7e56afa8b8e7f661c5a64d275e50d
Carlos A Santos <carlos.itdeveloper@gmail.com>
2022-01-29 00:04:44 -0300

    ApplyConfiguration and SetColumnType in OnModelCreating

--- src/Bookstore.Infra/Data/Orm/ApplicationDbContext.cs
+++ src/Bookstore.Infra/Data/Orm/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Bookstore.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
+using System.Linq;
 
 namespace Bookstore.Infra.Data.Orm
 {
@@ -11,5 +12,25 @@ namespace Bookstore.Infra.Data.Orm
         }
 
         public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            // Onde não tiver setado varchar e a propriedade for do tipo string fica valendo varchar(valor)
+            foreach (var property in modelBuilder.Model.GetEntityTypes()
+                .SelectMany(e => e.GetProperties()
+                    .Where(p => p.ClrType == typeof(string))))
+            {
+                //property.Relational().ColumnType = "varchar(80)"; Até o .Net 2.2
+                property.SetColumnType("varchar(80)");
+            }
+
+
+            // Busca os Mapppings de uma vez só
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+
+            base.OnModelCreating(modelBuilder);
+        }
+
     }
 }
